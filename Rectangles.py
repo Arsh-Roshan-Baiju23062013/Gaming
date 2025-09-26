2cd33a6bb879658564826aa0954e4630f4c1664d
Arsh-Roshan-Baiju23062013 <arshroshan23@gmail.com>
2025-09-26 17:13:15 +0400

    demo

---
+++ Rectangles.py
@@ -0,0 +1,22 @@
+import pgzrun
+import random
+WIDTH=600
+HEIGHT=600
+def draw():
+    screen.fill("black")
+    for i in range(20):
+        width = 150 - (i * 15)
+        height = 50 + (i * 15)
+        x = random.randint(0, WIDTH - width)
+        y = random.randint(0, HEIGHT - height)
+        r1=Rect((400, 200), (width, height))
+        r1.center=300, 300
+        r=random.randint(50, 255)
+        g=random.randint(50, 255)
+        b=random.randint(50, 255)
+        color=(r, g, b)
+        screen.draw.rect(r1, color)
+# def update():
+#     pass
+pgzrun.go()
+
